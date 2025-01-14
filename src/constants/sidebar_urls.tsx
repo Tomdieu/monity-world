d37590962f86f6d6e977f5c3bb532bef5e8047c6
Tomdieu <ivantomdio@gmail.com>
2025-01-14 16:35:32 +0100

    Add sidebar URLs and icons for navigation

---
+++ src/constants/sidebar_urls.tsx
@@ -0,0 +1,53 @@
+import { LayoutDashboard,ArrowRightLeft } from 'lucide-react'
+import { LuUsersRound } from "react-icons/lu";
+import { BsPiggyBank } from "react-icons/bs";
+import { CiCreditCard1 } from "react-icons/ci";
+import Image from "next/image";
+import { CiDollar } from "react-icons/ci";
+import { BiSupport } from "react-icons/bi";
+const ProjectIcon = ({className}:{className?:string})=><Image src={"/icons/project-icon.png"} alt={"project icon"} width={"24"} height={24}  className={className}/>
+
+const FinanceIcon = ({className}:{className?:string})=><Image src={"/icons/finance-icon.png"} alt={"finance icon"} width={"24"} height={24}  className={className}/>
+
+export const sidebarUrls = [
+    {
+        "title": "Dashboard",
+        "icon": LayoutDashboard,
+        "url": "/dashboard"
+    },
+    {
+        "title": "Profile",
+        "icon": LuUsersRound,
+        "url": "/dashboard/profile"
+    },
+    {
+        "title": "Transactions",
+        "icon": ArrowRightLeft,
+        "url": "/dashboard/transactions"
+    },
+    {
+        "title":"Savings",
+        "icon": BsPiggyBank,
+        "url":"/dashboard/savings"
+    },
+    {
+        "title":"Cards",
+        "icon": CiCreditCard1,
+        "url":"/dashboard/cards"
+    },
+    {
+        "title":"Projects",
+        "icon": ProjectIcon,
+        "url":"/dashboard/projects"
+    },
+    {
+        "title":"Finance",
+        "icon": CiDollar,
+        "url":"/dashboard/finance"
+    },
+    {
+        "title":"Support",
+        "icon": BiSupport,
+        "url":"/dashboard/support"
+    }
+]
