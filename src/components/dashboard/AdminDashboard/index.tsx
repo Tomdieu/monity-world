b60657c7d2f3d910fd8d3078ba35dbd8f4af58bb
Tomdieu <ivantomdio@gmail.com>
2025-01-21 22:22:39 +0100

    feat: enhance AdminDashboardPage with additional components for user engagement and notifications

--- src/components/dashboard/AdminDashboard/index.tsx
+++ src/components/dashboard/AdminDashboard/index.tsx
@@ -15,12 +15,24 @@ import {
     SelectValue,
 } from "@/components/ui/select"
 import { UserGrowth } from '../UserGrowth';
+import TopActiveUsers from '../TopActiveUsers';
+import TaskNotification from '../TaskNotification';
+import { sampleNotifications } from '@/constants/notifications';
+import Notes from '../Notes';
+import AlertsPanel from '../AlertsPanel';
+import { alerts, transactions } from '@/constants/data';
+import RecentTransactions from '../RecentTransactions';
 
 function AdminDashboardPage() {
+    const users = [
+        { id: 1, name: "Nguh Fabrice", points: 143, avatar: "/images/img1.png" },
+        { id: 2, name: "Meghan Jess", points: 60, avatar: "/avatar2.png" },
+        { id: 3, name: "Alex Turner", points: 28, avatar: "/avatar3.png" },
+    ];
     return (
-        <div className={"flex flex-col w-full h-full container mx-auto py-10"}>
+        <div className={"flex flex-col w-full h-full mx-auto py-10 px-10"}>
             <div className='w-full h-full flex gap-5'>
-                <div className='w-10/12 flex flex-col gap-5'>
+                <div className='w-9/12 flex flex-col gap-5 overflow-auto'>
                     <div className={"grid grid-cols-5 items-center gap-6 "}>
                         <div className={"flex flex-col gap-3 bg-white shadow-md rounded-lg p-3 w-full"}>
                             <div className={"flex gap-10 items-center justify-between"}>
@@ -108,8 +120,8 @@ function AdminDashboardPage() {
                             </div>
                         </div>
                     </div>
-                    <div className='grid grid-cols-12'>
-                        <div className='p-10 gap-5 flex flex-col col-span-7 bg-white rounded-lg'>
+                    <div className='grid grid-cols-12 gap-8'>
+                        <div className='p-4 gap-5 h-full flex flex-col col-span-7 bg-white rounded-lg'>
                             <div className='flex items-center justify-between'>
                                 <h5 className='text-2xl font-bold'>User Growth</h5>
                                 <Select>
@@ -125,18 +137,31 @@ function AdminDashboardPage() {
                                 </Select>
 
                             </div>
-                            <UserGrowth/>
+                            <UserGrowth />
 
                         </div>
-                        <div>
-
+                        <div className='flex w-full h-full flex-col col-span-5'>
+                            <TopActiveUsers title="Top 5 Active Users"
+                                users={users}
+                                onLeaderboardClick={() => { }} />
                         </div>
+                    </div>
+                    <div className=''>
+
+                        <RecentTransactions transactions={transactions}
+                            totalTransactions={1253}
+                            currentPage={1}
+                            onPageChange={(page) => console.log('Page changed:', page)} />
                     </div>
 
                 </div>
 
                 <Separator orientation='vertical' />
-                <div className='h-full w-2/12'></div>
+                <div className='h-full w-3/12 flex flex-col gap-2 overflow-auto scr'>
+                    <TaskNotification title='Task Notication' groups={sampleNotifications} />
+                    <Notes onSave={() => { }} notificationCount={3} />
+                    <AlertsPanel alerts={alerts} unreadCount={3} onLoadMore={() => { }} />
+                </div>
             </div>
         </div>
     )
