cd9f78087bd300ecbaa7f28db47e8485184ddb1e
Tomdieu <ivantomdio@gmail.com>
2025-01-21 22:42:44 +0100

    feat: add sample notifications data for project invitations

---
+++ src/constants/notifications.ts
@@ -0,0 +1,59 @@
+export const sampleNotifications = [
+    {
+      date: "Today",
+      notifications: [
+        {
+          id: 1,
+          avatarColor: "#F87171", // Red
+          initials: "MC",
+          name: "Mala Csaba",
+          message: "has invited you to the project",
+          project: "O2aasman",
+          time: "2 min",
+          isNew: true,
+        },
+        {
+          id: 2,
+          avatarColor: "#34D399", // Green
+          initials: "MC",
+          name: "Mala Csaba",
+          message: "has invited you to the project",
+          project: "O2aasman",
+          time: "10 min",
+          isNew: true,
+        },
+        {
+          id: 3,
+          avatarColor: "#60A5FA", // Blue
+          initials: "MC",
+          name: "Mala Csaba",
+          message: "has invited you to the project",
+          project: "O2aasman",
+          time: "1 hr",
+        },
+      ],
+    },
+    {
+      date: "Mon 9 Aug, 2021",
+      notifications: [
+        {
+          id: 4,
+          avatarColor: "#F87171", // Red
+          initials: "MC",
+          name: "Mala Csaba",
+          message: "has invited you to the project",
+          project: "O2aasman",
+          time: "12:05 pm",
+        },
+        {
+          id: 5,
+          avatarColor: "#8B5CF6", // Purple
+          initials: "MC",
+          name: "Mala Csaba",
+          message: "has invited you to the project",
+          project: "O2aasman",
+          time: "10:02 am",
+        },
+      ],
+    },
+  ];
