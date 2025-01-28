6bce9806fb81f40229651303296db3ba0dae52c7
Tomdieu <ivantomdio@gmail.com>
2025-01-28 23:36:09 +0100

    refactor: standardize quotation marks in type definitions and enhance TransactionStatus options

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,23 +1,22 @@
-type TransactionStatus = 'Completed' | 'Failed' | 'Flagged' | 'Cancelled';
+type TransactionStatus = "Completed" | "Failed" | "Flagged" | "Cancelled";
 
-type UserStatus = 'Active' | 'Inactive';
-type VerificationStatus = 'Verified' | 'UnVerified' | 'Pending';
-type RequestStatus = 'Pending' | 'Approved' | 'Rejected';
-type TransactionStatus = 'Completed' | 'Failed';
-type TransactionType = 'Transfer' | 'Recieved' | 'Savings' | 'Card recharge';
-type RemarkStatus = 'Processing' | 'Declined' | 'Good' | 'Scam attempt';
+type UserStatus = "Active" | "Inactive";
+type VerificationStatus = "Verified" | "UnVerified" | "Pending";
+type RequestStatus = "Pending" | "Approved" | "Rejected";
+type TransactionStatus = "Completed" | "Failed" | "Pending";
+type TransactionType = "Transfer" | "Recieved" | "Savings" | "Card recharge";
+type RemarkStatus = "Processing" | "Declined" | "Good" | "Scam attempt";
 
 type Transaction = {
-    id: string;
-    type: TransactionType;
-    dateTime: string;
-    amount: number;
-    recipient: {
-      name: string;
-      accountId?: string;
-      avatar?: string;
-    };
-    status: TransactionStatus;
-    remark: string;
+  id: string;
+  type: TransactionType;
+  dateTime: string;
+  amount: number;
+  recipient: {
+    name: string;
+    accountId?: string;
+    avatar?: string;
   };
-  
+  status: TransactionStatus;
+  remark: string;
+};
