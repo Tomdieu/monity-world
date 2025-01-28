d122ec62631a412d105e7ea9621a182e2434a31a
Tomdieu <ivantomdio@gmail.com>
2025-01-28 23:35:39 +0100

    feat: implement TransactionsChatPage with message selection functionality

--- src/app/dashboard/transactions/chats/page.tsx
+++ src/app/dashboard/transactions/chats/page.tsx
@@ -1,8 +1,28 @@
+"use client"
+import TransactionChats from '@/components/dashboard/Transactions/Chats';
+import { sampleMessages } from '@/constants/data';
 import React from 'react'
 
 function TransactionsChatPage() {
+  const [selectedMessages, setSelectedMessages] = React.useState<string[]>([]);
   return (
-    <div>TransactionsChatPage</div>
+    <div>
+      <TransactionChats
+      messages={sampleMessages}
+      currentPage={1}
+      totalItems={78}
+      onPageChange={(page) => console.log('Page:', page)}
+      onToggleStar={(id) => console.log('Toggle star:', id)}
+      onCheckMessage={(id, checked) => {
+        if (checked) {
+          setSelectedMessages(prev => [...prev, id]);
+        } else {
+          setSelectedMessages(prev => prev.filter(msgId => msgId !== id));
+        }
+      }}
+      selectedMessages={selectedMessages}
+    />
+    </div>
   )
 }
 
