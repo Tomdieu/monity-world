36d8856ff6628e86551710c35c669bb245d580ae
Tomdieu <ivantomdio@gmail.com>
2025-01-30 22:04:34 +0100

    feat: add SavingsLayout component for finance dashboard layout

---
+++ src/app/dashboard/finances/layout.tsx
@@ -0,0 +1,15 @@
+import FinancesSidebar from '@/components/dashboard/Finances/Sidebar'
+import React, { PropsWithChildren } from 'react'
+
+function SavingsLayout({children}:PropsWithChildren) {
+  return (
+    <div className='flex gap-2 w-full h-full'>
+        <FinancesSidebar className='w-2/12'/>
+        <div className='w-10/12 flex flex-col h-full overflow-y-auto'>
+            {children}
+        </div>
+    </div>
+  )
+}
+
+export default SavingsLayout
