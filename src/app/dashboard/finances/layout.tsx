341bfc95490adf88a911effde4d129c38717f415
Tomdieu <ivantomdio@gmail.com>
2025-02-03 14:56:59 +0100

    fix: adjust layout padding in SavingsLayout for improved spacing

--- src/app/dashboard/finances/layout.tsx
+++ src/app/dashboard/finances/layout.tsx
@@ -5,7 +5,7 @@ function SavingsLayout({children}:PropsWithChildren) {
   return (
     <div className='flex gap-2 w-full h-full'>
         <FinancesSidebar className='w-2/12'/>
-        <div className='w-10/12 flex flex-col h-full overflow-y-auto'>
+        <div className='w-10/12 flex flex-1 flex-col h-full overflow-y-auto pb-50'>
             {children}
         </div>
     </div>
