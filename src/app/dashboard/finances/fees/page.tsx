ece64bb833cbd111be3f4b944b4471c95c5b2ccf
Tomdieu <ivantomdio@gmail.com>
2025-02-03 14:58:27 +0100

    feat: refactor FinanceFeesPage component for improved structure and navigation

--- src/app/dashboard/finances/fees/page.tsx
+++ src/app/dashboard/finances/fees/page.tsx
@@ -1,20 +1,37 @@
+"use client";
+import {
+  ConfirmPasscodeDialog,
+  DiscardDialog,
+  ExitDialog,
+} from "@/components/dashboard/Finances/FinanceDialogs";
 import TransactionFees, {
   sampleFees,
 } from "@/components/dashboard/Finances/TransactionFees";
-import React from "react";
+import { useRouter } from "next/navigation";
+import React, { useState } from "react";
 
-function page() {
+function FinanceFeesPage() {
+  const router = useRouter();
+  const [isExitOpen, setIsExit] = useState(false);
   return (
-    <div>
+    <div className="w-full h-full">
       <TransactionFees
         cardTransactionFee={sampleFees.cardTransactionFee}
         projectFundingFee={sampleFees.projectFundingFee}
         withdrawalCharges={sampleFees.withdrawalCharges}
         sendingCharges={sampleFees.sendingCharges}
         savingsManagementFee={sampleFees.savingsManagementFee}
+        onReturn={() => setIsExit(true)}
       />
+      {/* <ConfirmPasscodeDialog open onCancel={()=>{}} onConfirm={()=>{}}/> */}
+      <ExitDialog
+        open={isExitOpen}
+        onCancel={() => setIsExit(false)}
+        onConfirm={()=>router.back()}
+      />
+      <div className="pb-20"></div>
     </div>
   );
 }
 
-export default page;
+export default FinanceFeesPage;
