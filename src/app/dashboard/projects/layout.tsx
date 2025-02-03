c03d0ba342f50a2e1fccdd39e38b8c7ee0bd0202
Tomdieu <ivantomdio@gmail.com>
2025-02-03 16:08:48 +0100

    feat: rename SavingsLayout to ProjectLayout and update styles for improved layout

--- src/app/dashboard/projects/layout.tsx
+++ src/app/dashboard/projects/layout.tsx
@@ -1,15 +1,15 @@
 import ProjectSidebar from '@/components/dashboard/Projects/Sidebar'
 import React, { PropsWithChildren } from 'react'
 
-function SavingsLayout({children}:PropsWithChildren) {
+function ProjectLayout({children}:PropsWithChildren) {
   return (
     <div className='flex gap-2 w-full h-full'>
         <ProjectSidebar className='w-2/12'/>
-        <div className='w-10/12 flex flex-col h-full overflow-y-auto'>
+        <div className='w-10/12 flex flex-1 pb-20 flex-col overflow-y-auto bg-slate-50'>
             {children}
         </div>
     </div>
   )
 }
 
-export default SavingsLayout
+export default ProjectLayout
