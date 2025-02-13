1d3bacb40c3828aae7e3cfa6f9ec7f8aa2d5203b
Tomdieu <ivantomdio@gmail.com>
2025-02-13 20:39:28 +0100

    feat: add additional font weights for Inter font in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,38 @@ const Inter = localFont({
   src: [
     {
       path: "../../public/fonts/Inter-VariableFont_opsz,wght.ttf",
+      weight: "300",
+      style: "light",
+    },
+    {
+      path: "../../public/fonts/Inter-VariableFont_opsz,wght.ttf",
       weight: "400",
       style: "normal",
+    },
+    {
+      path: "../../public/fonts/Inter-VariableFont_opsz,wght.ttf",
+      weight: "500",
+      style: "medium",
+    },
+    {
+      path: "../../public/fonts/Inter-VariableFont_opsz,wght.ttf",
+      weight: "600",
+      style: "semibold",
+    },
+    {
+      path: "../../public/fonts/Inter-VariableFont_opsz,wght.ttf",
+      weight: "700",
+      style: "bold",
+    },
+    {
+      path: "../../public/fonts/Inter-VariableFont_opsz,wght.ttf",
+      weight: "800",
+      style: "extrabold",
+    },
+    {
+      path: "../../public/fonts/Inter-VariableFont_opsz,wght.ttf",
+      weight: "900",
+      style: "black",
     },
   ],
   variable: "--font-inter",
